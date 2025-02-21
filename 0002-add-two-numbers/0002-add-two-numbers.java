a955fb0af5f449459bd717f9974adc4a99b80a66
Aakarsh Mishra <108521390+AakarshMishra@users.noreply.github.com>
2025-02-21 19:47:47 +0530

    Time: 1 ms (100%), Space: 44.5 MB (51.96%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,31 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy  = new ListNode(0);
-        ListNode l3 = dummy;
-        int carry = 0;
-        int currSum = 0;
-        while(l1!=null||l2!=null){
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-
-            currSum = val1+val2+carry;
-            carry = currSum/10;
-            int node = currSum%10;
-             
-            ListNode newNode = new ListNode(node);
-            l3.next = newNode;
-
-            if(l1!=null) l1 = l1.next;
-            if(l2!=null) l2 = l2.next;
-            l3 = l3.next;
+        ListNode dummy=new ListNode(0);
+        ListNode l3=dummy;
+        int carry=0;
+        while(l1!=null && l2!=null)
+        {
+            int sum=l1.val+l2.val+carry;
+            carry=sum/10;
+            ListNode newn=new ListNode(sum%10);
+            l3.next=newn;
+            l3=l3.next;
+            l1=l1.next;
+            l2=l2.next;
         }
-        if(carry>0){
-            ListNode newNode = new ListNode(carry);
-            l3.next = newNode;
-            l3 = l3.next;
+        while(l1!=null)
+        {
+            int sum=l1.val+carry;
+            carry=sum/10;
+            ListNode newn=new ListNode(sum%10);
+            l3.next=newn;
+            l3=l3.next;
+            l1=l1.next;
         }
-
+        while(l2!=null)
+        {
+            int sum=l2.val+carry;
+            carry=sum/10;
+            ListNode newn=new ListNode(sum%10);
+            l3.next=newn;
+            l3=l3.next;
+            l2=l2.next;
+        }
+        if(carry!=0)
+        {
+        ListNode newn=new ListNode(carry);
+            l3.next=newn;
+        }
         return dummy.next;
     }
 }
