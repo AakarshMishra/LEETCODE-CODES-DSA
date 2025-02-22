d292f2b7e141334272e8a231d256bedba8a8992e
Aakarsh Mishra <108521390+AakarshMishra@users.noreply.github.com>
2025-02-22 22:55:03 +0530

    Time: 1 ms (34.08%), Space: 45.6 MB (29.82%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -1,22 +1,18 @@
 class Solution {
-    public boolean checkPossibility(int[] arr) {
+    public boolean checkPossibility(int[] nums) {
         int c=0;
-        int n=arr.length;
-		for(int x=0;x<(n-1);x++)
-		{
-			if(!(arr[x]<=arr[x+1]))
-			{
-				c++;
-				if(x>0 && arr[x-1]>arr[x+1])
-				{
-					arr[x+1]=arr[x];
-				}
-			
-				if(c==2)
-				return false;
-
-			}
-		}
-		return true;
+        for(int x=0;x<nums.length-1;x++)
+        {
+            if(nums[x]>nums[x+1])
+            {
+                c++;
+                if(x>0 && nums[x-1]>nums[x+1])
+                nums[x+1]=nums[x];
+                if(c>1)
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
